234095251543753931b709a71dd63d2b1a200563
Letícia Campana <84405478+Westemaier@users.noreply.github.com>
2021-12-14 20:38:57 -0300

    ByteBank2
    Inicio do ByteBank2

---
+++ ByteBank2/ByteBank2/Program.cs
@@ -0,0 +1,33 @@
+﻿using ByteBank2.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+
+namespace ByteBank2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario Carlos = new Funcionario (1);
+
+            Carlos.Nome = "Carlos";
+            Carlos.CPF = "546.879.157-20";
+            Carlos.Salario = 2000;
+            
+            
+            
+            Console.WriteLine(Carlos.Nome);
+            Console.WriteLine(Carlos.GetBoficacao());
+            
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}

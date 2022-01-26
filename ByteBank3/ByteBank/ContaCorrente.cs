9c625ec7b475c8bbcd28f6c29d0e3cec22bf221c
Letícia Campana <84405478+Westemaier@users.noreply.github.com>
2022-01-26 17:04:44 -0300

    ByteBank3
    ByteBank3

--- ByteBank3/ByteBank/ContaCorrente.cs
+++ ByteBank3/ByteBank/ContaCorrente.cs
@@ -35,6 +35,8 @@ namespace ByteBank
             }
         }
 
+        public double Valor { get; private set; }
+
         public ContaCorrente(int agencia, int numero)
         {
             if (numero <= 0)
@@ -56,9 +58,14 @@ namespace ByteBank
 
         public void Sacar(double valor)
         {
+            if (valor < 0)
+            {
+                throw new ArgumentException("Valor invalido para o saque.", nameof(valor));
+             }
+
             if (_saldo < valor)
             {
-                throw new SaldoinsuficienteException("Saldo insuficiente para o saque no valor de " + valor);
+                throw new SaldoinsuficienteException(Saldo, valor);
             }
 
             _saldo -= valor;
@@ -70,16 +77,15 @@ namespace ByteBank
             _saldo += valor;
         }
 
-        public bool Transferir(double valor, ContaCorrente contaDestino)
+        public void Transferir(double valor, ContaCorrente contaDestino)
         {
-            if (_saldo < valor)
+            if (valor < 0)
             {
-                return false;
+                throw new ArgumentException("Valor invalido para a Transferencia.", nameof(valor));
             }
-
-            _saldo -= valor;
-            contaDestino.Depositar(valor);
-            return true;
+            Sacar(valor);
+    
+            contaDestino.Depositar(valor);        
         }
     }
 }
--- ByteBank3/ByteBank/SaldoinsuficienteException.cs
+++ ByteBank3/ByteBank/SaldoinsuficienteException.cs
@@ -8,16 +8,23 @@ namespace ByteBank
 {
     public class SaldoinsuficienteException : Exception
     {
+        public double Saldo { get; }
+        public double ValorSaque { get; }
 
         public SaldoinsuficienteException()
         {
 
         }
+        public SaldoinsuficienteException(double saldo, double valorSaque)
+            : this("Tentativa de saque do valor de " + valorSaque + " em uma conta com saldo de " + saldo)
+        {
+            this.Saldo = saldo;
+            ValorSaque = valorSaque;
+        }
 
         public SaldoinsuficienteException (string mensagem)
             : base (mensagem)        
         {
-
 
         }
     }

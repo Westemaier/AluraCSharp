8a0b39b18bb4ea7f24af73e28908a18b662c4ade
Letícia Campana <84405478+Westemaier@users.noreply.github.com>
2022-02-01 15:15:48 -0300

    ExtratorValorURL ByteBank Refatoração

---
+++ ByteBankSA/ByteBank/ByteBank.SistemaAgencia/ExtratorValordeArgumentosURL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+     public class ExtratorValordeArgumentosURL
+    {
+        private readonly string _argumentos;
+        public string URL { get; }
+        public ExtratorValordeArgumentosURL(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                throw new ArgumentException("O Argumento url não pode ser nulo ou vazio.", nameof(url));
+            }
+            URL = url;
+            int indiceInterrogacao = URL.IndexOf('?');
+            _argumentos = url.Substring(indiceInterrogacao + 1);
+        }
+        public string GetValor(string nomeParametro)
+        {
+            return "";
+            //int indiceParametro = _argumentos.IndexOf;
+        }      
+    }
+}
--- ByteBankSA/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ByteBankSA/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -9,12 +9,34 @@ namespace ByteBank.SistemaAgencia
         {
             //pagina?argumentos
             //012345678
-            string url = "paginas?argumentos";
+            string palavra = "pagina?moedaOrigem=real&moedaDestino=dolar";
+            string nomeArgumento = "moedaDestino";
+            int indice = palavra.IndexOf(nomeArgumento);
+            Console.WriteLine(indice);
+            Console.WriteLine("Tamanho da String nomeArgumento " + nomeArgumento.Length);
+            Console.WriteLine(palavra);
+            Console.WriteLine(palavra.Substring(indice));
+            Console.WriteLine(palavra.Substring(indice + nomeArgumento.Length + 1));
+            Console.ReadLine();
+            string textoVazio = "";
+            string textoNulo = null;
+            string textoQualquer = "sabydgsadasdas";
+            Console.WriteLine(String.IsNullOrEmpty(textoVazio));
+            Console.WriteLine(String.IsNullOrEmpty(textoNulo));
+            Console.WriteLine(String.IsNullOrEmpty(textoQualquer));
+            Console.ReadLine();
+            ExtratorValordeArgumentosURL extrator = new ExtratorValordeArgumentosURL(null);
+            string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
+            int indiceInterrogacao = url.IndexOf('?');
+            Console.WriteLine(indiceInterrogacao);
             Console.WriteLine(url);
-            string argumentos = url.Substring(8);
-            Console.WriteLine(argumentos);      
+            string argumentos = url.Substring(indiceInterrogacao + 1);
+            Console.WriteLine(argumentos);     
             Console.ReadLine();
         }
         
     }
+
+
+
 }
